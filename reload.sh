24c2de14b8a6b74fd3a09563e6dbe4a436c85226
root <root@mon.home.local>
2021-11-14 23:04:16 +0000

    moved grafana to volume, cleaned up prometheus config

--- reload.sh
+++ reload.sh
@@ -1,10 +1,6 @@
 #alertmanager
 curl -X POST http://localhost:9093/-/reload
-#prom snmp
-curl -X POST http://localhost:9001/-/reload
-#prom ceph
-curl -X POST http://localhost:9002/-/reload
-#prom other
+#prom oneoff
 curl -X POST http://localhost:9003/-/reload
 #prom selfmon
 curl -X POST http://localhost:9004/-/reload
