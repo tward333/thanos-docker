4e05fc8e2dfe24f5b17bd13aab33f76234173e4e
tward <22226719+tward333@users.noreply.github.com>
2021-11-14 18:56:22 -0500

    forgot to rename volume from test

--- importvol.sh
+++ importvol.sh
@@ -1,10 +1,10 @@
 
 #nuke existing vol -- optional
-docker volume rm prometheus_grafana-storage2
+#docker volume rm prometheus_grafana-storage
 #do the import
-docker volume create prometheus_grafana-storage2
+docker volume create prometheus_grafana-storage
 voltar=$(ls grafana*.tar)
-docker run --rm -v prometheus_grafana-storage2:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3"
+docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3"
 
 #inspect the result -- optional
-docker run --rm -it -v prometheus_grafana-storage:/data1  -v prometheus_grafana-storage2:/data2  -v $(pwd):/backup ubuntu bash 
+#docker run --rm -it -v prometheus_grafana-storage:/data1  -v prometheus_grafana-storage2:/data2  -v $(pwd):/backup ubuntu bash 
