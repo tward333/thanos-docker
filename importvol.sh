19c5aee31a70e1c200d7e26108c49578631d18c3
tward <22226719+tward333@users.noreply.github.com>
2021-11-24 22:41:39 -0500

    fixed permission issue on importvol.sh

--- importvol.sh
+++ importvol.sh
@@ -4,7 +4,7 @@
 #do the import
 docker volume create prometheus_grafana-storage
 voltar=$(ls grafana*.tar)
-docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3"
+docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3; chown -R 472.472 /data"
 
 #inspect the result -- optional
 #docker run --rm -it -v prometheus_grafana-storage:/data1  -v prometheus_grafana-storage2:/data2  -v $(pwd):/backup ubuntu bash 
