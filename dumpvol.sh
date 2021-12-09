33ef073875a9f1976c8a81876c15256d835da412
tward <22226719+tward333@users.noreply.github.com>
2021-12-09 01:23:53 -0500

    importvol and dumpvol now use s3

--- dumpvol.sh
+++ dumpvol.sh
@@ -1,5 +1,12 @@
-#do the dump
+#do the volume dump
 date=$(date +%Y%m%d%H%M)
-docker run --rm --volumes-from grafana2 -v $(pwd):/backup ubuntu tar cvf /backup/grafana.$date.tar /var/lib/grafana/
+backupfile=grafana.$date.tar
+docker run --rm --volumes-from grafana2 -v $(pwd):/backup ubuntu tar cvf /backup/$backupfile /var/lib/grafana/
 #upload the dump to repo
 #rsync -avP grafana*.tar root@192.168.5.1:/opt/docker/tmpstore
+rclone copy $backupfile locals3:/deployblob/prometheus/
+
+
+#copy prometheus data directly to repo
+rclone copy  ./data/prometheus/other/ locals3:/deployblob/prometheus/data/
+
--- importvol.sh
+++ importvol.sh
@@ -1,10 +1,17 @@
-
+latesttar=$(rclone ls locals3:/deployblob/prometheus/|tail -1|awk {'print $2'})
+rclone copy locals3:/deployblob/prometheus/$latesttar .
 #nuke existing vol -- optional
 #docker volume rm prometheus_grafana-storage
 #do the import
 docker volume create prometheus_grafana-storage
-voltar=$(ls grafana*.tar)
-docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3; chown -R 472.472 /data"
+#voltar=$(ls grafana*.tar)
+#docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$voltar -C /data --strip 3; chown -R 472.472 /data"
+
+docker run --rm -v prometheus_grafana-storage:/data  -v $(pwd):/backup ubuntu bash -c "tar xvf /backup/$latesttar -C /data --strip 3; chown -R 472.472 /data"
 
 #inspect the result -- optional
 #docker run --rm -it -v prometheus_grafana-storage:/data1  -v prometheus_grafana-storage2:/data2  -v $(pwd):/backup ubuntu bash 
+
+#pull down prometheus datadir directly
+rclone copy locals3:/deployblob/prometheus/data/ ./data/prometheus/other/
+~                                                                                
